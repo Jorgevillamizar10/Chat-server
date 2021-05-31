ab103d1430e4713b28774e03ee877239299572a3
Jorge Villamizar <jorgevillamizar14@gmail.com>
2021-05-30 22:31:56 -0400

    new endpoint for messages

--- models/sockets.js
+++ models/sockets.js
@@ -1,31 +1,24 @@
-
-
 class Sockets {
+	constructor(io) {
+		this.io = io;
 
-    constructor( io ) {
+		this.socketEvents();
+	}
 
-        this.io = io;
-
-        this.socketEvents();
-    }
-
-    socketEvents() {
-        // On connection
-        this.io.on('connection', ( socket ) => {
-
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-                
-                this.io.emit('mensaje-from-server', data );
-            });
-            
-        
-        });
-    }
-
-
+	socketEvents() {
+		// On connection
+		this.io.on("connection", (socket) => {
+			// TODO: Validar el JWT
+			// Si el token no es valido, desconectar
+			// TODO: Saber que usuario esta activo mediante el uId
+			// TODO: Emitir todos los usuarios conectados
+			// TODO: Socket join
+			// TODO: Escuchar cuando el cliente manda un mensaje
+			//mensaje-personal
+			// TODO: Disconnect
+			//macar en la BD que el usuario se desconecto
+		});
+	}
 }
-
 
 module.exports = Sockets;
